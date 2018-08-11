1fc4d6ddcdea191d3a31b4bc7ec2947fd33ef35e
kvavilal1 <40915079+kvavilal1@users.noreply.github.com>
2018-08-11 09:15:48 +0530

    Add files via upload

---
+++ CaseStudy31/src/com/cg/faculty/exceptions/FacultyDetailsNotFoundException.java
@@ -0,0 +1,38 @@
+package com.cg.faculty.exceptions;
+
+public class FacultyDetailsNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public FacultyDetailsNotFoundException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FacultyDetailsNotFoundException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FacultyDetailsNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FacultyDetailsNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FacultyDetailsNotFoundException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+}
